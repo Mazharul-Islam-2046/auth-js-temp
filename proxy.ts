63cdcfc84ee0b6a3ae48d7ed81ca5a304740955d
Mazharul-Islam-2046 <mdmazharulislam2046@gmail.com>
2026-02-09 22:07:55 +0600

    chore(Auth): Configure Redirect

--- proxy.ts
+++ proxy.ts
@@ -5,20 +5,24 @@ import { NextResponse } from "next/server"
 const { auth: proxy } = NextAuth(authConfig)
 
 const privateRoutes = [
+    "/",
     "/dashboard",
     ]
 
 export default proxy(async (req) => {
     const isLoggedIn = !!req.auth;
+    console.log("Req AUth",req.auth)
     const {nextUrl} = req;
 
     const isPrivateRoute = privateRoutes.includes(nextUrl.pathname);
     const isAuthRoute = nextUrl.pathname.includes("/auth");
     const isApiRoute = nextUrl.pathname.includes("/api");
 
+    // console.log("Is Auth",isPrivateRoute, "isLoggedIn", isLoggedIn)
 
-    if(isApiRoute) return Promise.resolve();
-    if(isAuthRoute) return Promise.resolve();
+
+    if(isApiRoute && isLoggedIn) return Promise.resolve();
+    if(isAuthRoute && isLoggedIn) return NextResponse.redirect(new URL('/dashboard', req.url));
     if(isPrivateRoute && !isLoggedIn) return NextResponse.redirect(new URL('/auth/signin', req.url));
     if(!isPrivateRoute && isLoggedIn) return NextResponse.redirect(new URL('/dashboard', req.url));
     return Promise.resolve();
